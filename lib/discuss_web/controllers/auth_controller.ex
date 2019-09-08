c8e9c5e1a9ec9dcc08f3e4cd2b199a2d8b3a875b
Victor Lellis <vmlellis@gmail.com>
2019-09-08 02:23:49 -0300

    Sign In / Sign Out

--- lib/discuss_web/controllers/auth_controller.ex
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -5,25 +5,20 @@ defmodule DiscussWeb.AuthController do
   alias Discuss.Accounts
 
   def callback(conn, _params) do
-    # IO.puts "++++++++"
-    # IO.inspect(conn.assigns)
-    # IO.puts "++++++++"
-    # IO.inspect(params)
-    # IO.puts "++++++++"
-
     %{assigns: %{ueberauth_auth: auth}} = conn
-    # IO.puts "++++++++"
-    # IO.inspect(auth)
-    # IO.puts "++++++++"
     user_params = %{
       token: auth.credentials.token,
       email: auth.info.email,
       provider: Atom.to_string(auth.provider)
     }
-    # IO.inspect(user_params)
-    # IO.puts "++++++++"
 
     conn |> signin(user_params)
+  end
+
+  def signout(conn, _params) do
+    conn
+    |> configure_session(drop: true)
+    |> redirect(to: Routes.topic_path(conn, :index))
   end
 
   defp signin(conn, user_params) do
@@ -33,10 +28,7 @@ defmodule DiscussWeb.AuthController do
         |> put_flash(:info, "Welcome back!")
         |> put_session(:user_id, user.id)
         |> redirect(to: Routes.topic_path(conn, :index))
-      {:error, reason} ->
-        IO.puts "++++++++"
-        IO.inspect(reason)
-        IO.puts "++++++++"
+      {:error, _reason} ->
         conn
         |> put_flash(:error, "Error signing in")
         |> redirect(to: Routes.topic_path(conn, :index))
